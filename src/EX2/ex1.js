f6abddeeda191f4c9814fb9e93f6630c7923ca3f
Jordi.BS202 <jordi.bargallo@cirvianum.cat>
2024-11-13 21:10:07 +0100

    ex 4 final

--- src/EX2/ex1.js
+++ src/EX2/ex1.js
@@ -14,18 +14,17 @@ const getData = async (url) => {
     }
 };
 
-const getRentPrices = async () => { const url = 'https://servicios.ine.es/wstempus/js/ES/DATOS_TABLA/59057?nult=10'; 
-    try { const data = await getData(url); 
-        // Filtrar les dades per obtenir només les que contenen "Nombre: Catalunya" 
-        const catalunya = data.filter((element) => element.Nombre.includes('Cataluña. Total')); 
-        // Extreure només els valors dels atributs dins de Data 
-        /*const valors = catalunya.map((element) => { return element.Data.map((item) => item.Valor); }); 
-        return valors;*/
-        
+const getRentPrices = async () => {
+    const url = 'https://servicios.ine.es/wstempus/js/ES/DATOS_TABLA/59057?nult=10';
+    try {
+        const data = await getData(url);
+        //Filtrem les dades per obtenir només les que contenen "Nombre: Catalunya"
+        const catalunya = data.filter((element) => element.Nombre.includes('Cataluña. Total'));
         return catalunya;
-    } catch (error) { 
-     console.error('Error:', error);  return[]; 
-    } 
+    } catch (error) {
+        console.error('Error:', error);
+        return [];
+    }
 };
 getRentPrices().then((rebo) => console.log(rebo));
 
@@ -63,21 +62,96 @@ const showRentPrices = (data) => {
 };
 
 const getIPC = async () => {
-    
+    //Fem la peticio a la API de l'IPC
+    const url = 'https://servicios.ine.es/wstempus/js/ES/DATOS_TABLA/59057?nult=10';
+    try {
+        const data = await getData(url);
+        //obtinc la referencia al meu selector a traves del DOM
+        const select = document.getElementById('ipc-selector');
+
+        data.forEach((element) => {
+            //Creo un element option per cada element de la llista
+            const option = document.createElement('option');
+            //Separo el nom de l'element per poder mostrar-lo
+            const nameParts = element.Nombre.split('.');
+            
+            const nameAfterFirtsDot = nameParts.slice(1).join('.');
+
+            // console.log(nameAfterFirtsDot);
+            option.value = nameAfterFirtsDot;
+            option.text = nameAfterFirtsDot;
+            select.appendChild(option);
+        });
+        return data;
+    } catch (error) {
+        console.error('Error:', error);
+        return [];
+    }
 }
 
 const showIPC = async () => {
+    
+    try {
+        const data = await getIPC();
+        const select = document.getElementById('ipc-selector');
+        const IPCselected = select.value;
+        let filteredData = data.find((element) => element.Nombre.includes(IPCselected));
+        console.log(filteredData);
+        //em  creo les dues array on afegire la llegenda els valors
+        const labels = [];
+        const values = [];
+
+        //recorro les dades per obtenir els valors i les dates
+        filteredData.Data.forEach((item) => {
+            labels.push(item.Anyo);
+            values.push(item.Valor);
+        });
+
+        labels.reverse();
+        values.reverse();
+        myChart(labels, values);
+    } catch (error) {
+        
+    }
 }
 
 let chart = null; // Declarem una variable global per a guardar el gràfic
 
-const myChart = () => {
+const myChart = (labels, data) => {
+    const ctx = document.getElementById('myChart').getContext('2d');
+    if (chart) {
+        chart.destroy();
+    }
+    chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'IPC',
+                data: data,
+                borderColor: 'rgb(255, 99, 132)',
+                borderWidth: 1,
+                fill: false
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: false
+                }
+            }
+        }
+    });
 }
 
 main = async () => {
     try {
         const data = await getRentPrices();
         showRentPrices(data);
+        showIPC();
+
+        const select = document.getElementById('ipc-selector');
+        select.addEventListener('change', showIPC);
     } catch (error) {
         console.error('Error:', error);
         throw error;        
